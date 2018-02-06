9cf3d3ac0cf941d450a37b57cae088e36922b242
Evangeline VIllena <vvanjie@yahoo.com>
2018-02-06 04:05:41 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,35 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element){
+  return [element, ...array];
+}
+addElementToBeginningOfArray(chocolateBars, "twix");
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+  return array;
+}
+addElementToEndOfArray(chocolateBars, "twix");
+
+function addElementToEndOfArray(array, element){
+  return [...array, element];
+}
+addElementToEndOfArray(chocolateBars, "twix");
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+  return array;
+}
+destructivelyAddElementToEndOfArray(chocolateBars, "twix");
+
+function accessElementInArray(array, index){
+  return array[index];
+}
+accessElementInArray(chocolateBars, [2]);
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift();
+  return array;
+}
+destructivelyRemoveElementFromBeginningOfArray(chocolateBars);
+
