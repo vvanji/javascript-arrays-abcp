23bfea11c2d6b82c97fd041a92120fd0261c37a2
Evangeline VIllena <vvanjie@yahoo.com>
2018-02-06 04:32:12 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -34,7 +34,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 destructivelyRemoveElementFromBeginningOfArray(chocolateBars);
 
 function removeElementFromBeginningOfArray(array){
-  array.slice(1);
+  array.slice(0);
   return array;
 }
 removeElementFromBeginningOfArray(chocolateBars);
